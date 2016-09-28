5c95dd83c0b1eceaaa32e25ebd86799617689361
François Raminosona <framinosona@hotmail.Fr>
2016-09-28 18:03:39 +0200

    TwitterClient & API

---
+++ Xamarin/TwitterClient/TwitterClient/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TwitterAPI;
+using Xamarin.Forms;
+
+namespace TwitterClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new TwitterClient.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            TwitterApi.Instance.SetCredentials(
+                TwitterCredentials.OAuthAccessToken,
+                TwitterCredentials.OAuthAccessTokenSecret,
+                TwitterCredentials.ConsumerKey,
+                TwitterCredentials.ConsumerSecret
+                );
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Xamarin/TwitterClient/TwitterClient/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TwitterAPI;
+using Xamarin.Forms;
+
+namespace TwitterClient
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}

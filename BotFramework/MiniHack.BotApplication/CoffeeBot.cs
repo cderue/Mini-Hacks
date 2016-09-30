f85a28e564b49c1284c2e28da8e1c697c36717d6
Edwige Seminara <edwige.seminara@live.com>
2016-09-30 02:31:47 +0200

    First commit

---
+++ BotFramework/MiniHack.BotApplication/CoffeeBot.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Json;
+using MiniHack.BotApplication.Resource;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace MiniHack.BotApplication
+{
+
+    public enum CoffeeOptions
+    {
+        Coffee, CaramelMacchiato, Mocha, Chocolate, Tea
+    };
+
+    public enum TemperatureOptions
+    {
+        Hot, Cold
+    };
+
+    public enum SizeOptions
+    {
+        Ristretto, Short, Medium, Long, Big
+    };
+
+    public enum SugarOptions
+    {
+        ALittleBitOfSugar, ALot
+    };
+
+    public enum ToppingOptions
+    {
+        [Terms("except", "but", "not", "no", "all", "everything")]
+        Cream, Milk, Vanilla, Chocolate, Caramel, Everything
+    };
+
+    [Serializable]
+    [Template(TemplateUsage.NotUnderstood, "I do not understand \"{0}\".", "Try again, I don't get \"{0}\".")]
+    [Template(TemplateUsage.EnumSelectOne, "What kind of {&} would you like on your coffee? {||}", ChoiceStyle = ChoiceStyleOptions.Buttons)]
+    public class CoffeeOrder
+    {
+        [Prompt("What kind of {&} would you like? {||}")]
+        public CoffeeOptions? Coffee;
+
+        [Prompt("Which temperature do you want? {||}")]
+        public TemperatureOptions? Temperature;
+
+        [Prompt("What size do you want? {||}")]
+        public SizeOptions? Size;
+
+        // An optional annotation means that it is possible to not make a choice in the field.
+        [Optional]
+        [Prompt("Choose your toppings ? {||}")]
+        [Template(TemplateUsage.NoPreference, "None")]
+        public List<ToppingOptions> Toppings { get; set; }
+
+        [Optional]
+        [Prompt("Do you want some sugar ? {||}")]
+        [Template(TemplateUsage.NoPreference, "None")]
+        public SugarOptions? Sugar { get; set; }
+
+        public string Name;
+
+        public string DeliveryAddress;
+
+        [Pattern(@"(\(\d{3}\))?\s*\d{3}(-|\s*)\d{4}")]
+        public string PhoneNumber;
+
+        [Optional]
+        [Template(TemplateUsage.StatusFormat, "{&}: {:t}", FieldCase = CaseNormalization.None)]
+        public DateTime? DeliveryTime;
+
+        [Numeric(1, 5)]
+        [Optional]
+        [Describe("your experience today")]
+        public double? Rating;
+
+
+        public static IForm<JObject> BuildJsonForm()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MiniHack.BotApplication.CoffeeBot.json"))
+            {
+                var schema = JObject.Parse(new StreamReader(stream).ReadToEnd());
+                return new FormBuilderJson(schema)
+                    .AddRemainingFields()
+                    .Build();
+            }
+        }
+
+        private static ConcurrentDictionary<CultureInfo, IForm<CoffeeOrder>> _forms = new ConcurrentDictionary<CultureInfo, IForm<CoffeeOrder>>();
+
+        public static IForm<CoffeeOrder> BuildLocalizedForm()
+        {
+            var culture = Thread.CurrentThread.CurrentUICulture;
+            IForm<CoffeeOrder> form;
+
+            if (!_forms.TryGetValue(culture, out form))
+            {
+                OnCompletionAsyncDelegate<CoffeeOrder> processOrder = async (context, state) =>
+                {
+                    await context.PostAsync(DynamicCoffee.Processing);
+                };
+
+                var builder = new FormBuilder<CoffeeOrder>()
+                        .Message(DynamicCoffee.WelcomeMessage)
+                        .Field(nameof(Coffee))
+                        .Field(nameof(Temperature))
+                        .Field(nameof(Size))
+                        .Field(nameof(Sugar))
+                        .Field(nameof(Toppings),
+                            validate: async (state, value) =>
+                            {
+                                if (value != null)
+                                {
+                                    var values = ((List<object>)value).OfType<ToppingOptions>();
+                                    var result = new ValidateResult { IsValid = true, Value = values };
+                                    if (values != null && values.Contains(ToppingOptions.Everything))
+                                    {
+                                        result.Value = (from ToppingOptions topping in Enum.GetValues(typeof(ToppingOptions))
+                                                        where topping != ToppingOptions.Everything && !values.Contains(topping)
+                                                        select topping).ToList();
+                                    }
+                                    return result;
+                                }
+                                else
+                                {
+                                    // To handle null value when choosing none in toppings
+                                    var result = new ValidateResult { IsValid = true, Value = null };
+                                    return result;
+                                }
+                            })
+                        .Confirm(async (state) =>
+                        {
+                            var cost = 0.0;
+                            switch (state.Size)
+                            {
+                                case SizeOptions.Ristretto: cost = 2.0; break;
+                                case SizeOptions.Short: cost = 3.49; break;
+                                case SizeOptions.Medium: cost = 5.0; break;
+                                case SizeOptions.Long: cost = 6.49; break;
+                                case SizeOptions.Big: cost = 8.99; break;
+                            }
+                            return new PromptAttribute($"Total for your coffee is {cost:C2} is that ok?");
+                        })
+                        .Confirm(async (state) =>
+                        {
+                            string customMessage = "Do you want to order your {Temperature} {Size} {Coffee} ";
+                            if (state.Sugar != null || state.Toppings != null)
+                            {
+                                customMessage = string.Concat(customMessage, "with {[{Sugar} {Toppings}]} ");
+                            }
+                            customMessage = string.Concat(customMessage, "?");
+                            return new PromptAttribute(customMessage);
+                        })
+                        //.AddRemainingFields() // Pour aller plus loin
+                        .Message("Thanks for ordering a coffee!")
+                        .OnCompletion(processOrder);
+
+                builder.Configuration.DefaultPrompt.ChoiceStyle = ChoiceStyleOptions.Auto;
+                form = builder.Build();
+                _forms[culture] = form;
+            }
+            return form;
+        }
+    }
+}
---
+++ BotFramework/MiniHack.BotApplication/Controllers/MessagesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace MiniHack.BotApplication
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                // one of these will have an interface and process it
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRootDialog);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
+        }
+
+        internal static IDialog<CoffeeOrder> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(CoffeeOrder.BuildLocalizedForm))
+            .Do(async (context, order) =>
+               {
+                   try
+                   {
+                       var completed = await order;
+                       // Actually process the coffee order...
+                       await context.PostAsync("Processed your order!");
+                   }
+                   catch (FormCanceledException<CoffeeOrder> e)
+                   {
+                       string reply;
+                       if (e.InnerException == null)
+                       {
+                           reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                       }
+                       else
+                       {
+                           reply = "Sorry, I've had a short circuit.  Please try again.";
+                       }
+                       await context.PostAsync(reply);
+                   }
+               });
+
+
+        }
+
+        internal static IDialog<JObject> MakeJsonRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(CoffeeOrder.BuildJsonForm))
+                .Do(async (context, order) =>
+                {
+                    try
+                    {
+                        var completed = await order;
+                        // Actually process the coffee order...
+                        await context.PostAsync("Processed your order!");
+                    }
+                    catch (FormCanceledException<JObject> e)
+                    {
+                        string reply;
+                        if (e.InnerException == null)
+                        {
+                            reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                        }
+                        else
+                        {
+                            reply = "Sorry, I've had a short circuit.  Please try again.";
+                        }
+                        await context.PostAsync(reply);
+                    }
+                });
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
